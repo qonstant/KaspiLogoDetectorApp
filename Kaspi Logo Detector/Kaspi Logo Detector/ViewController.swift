ac2de1830e6d1ef44bfb559ee87b004d8a8281b0
Rakymzhan Zhabagin <rakorako2204@gmail.com>
2024-04-23 22:05:21 +0500

    first commit

---
+++ Kaspi Logo Detector/Kaspi Logo Detector/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  Kaspi Logo Detector
+//
+//  Created by Rakymzhan Zhabagin on 23.04.2024.
+//
+
+import UIKit
+import CoreML
+import Vision
+import ImageIO
+import MessageUI
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
+
+    @IBOutlet weak var photoImageView: UIImageView?
+    @IBOutlet weak var confidence: UILabel!
+
+    lazy var detectionRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: Kaspi_Logo_Detector_1().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processDetections(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .scaleFit
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    var confidenceValue: Float = 0.0
+    
+    @IBAction func testPhoto(sender: UIButton) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    
+    private func updateDetections(for image: UIImage) {
+        let orientation = CGImagePropertyOrientation(rawValue: UInt32(image.imageOrientation.rawValue))
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation!)
+            do {
+                try handler.perform([self.detectionRequest])
+            } catch {
+                print("Failed to perform detection.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func sendEmailButtonTapped(_ sender: UIButton) {
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposeViewController = MFMailComposeViewController()
+            mailComposeViewController.mailComposeDelegate = self
+            mailComposeViewController.setToRecipients(["rakorako003@gmail.com"])
+            mailComposeViewController.setSubject("New Training Data")
+            mailComposeViewController.setMessageBody("Hi, I'm writing from Kaspi Logo Detector App, this is new training data", isHTML: false)
+            present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            // Show error message if device is unable to send email
+            print("Device cannot send email")
+        }
+    }
+
+    
+private func processDetections(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                print("Unable to detect anything.\n\(error!.localizedDescription)")
+                return
+            }
+            
+            let detections = results as! [VNRecognizedObjectObservation]
+            
+            // Extract the confidence value from the first detection
+            if let firstDetection = detections.first {
+                self.confidenceValue = firstDetection.labels.first?.confidence ?? 0.0
+                
+                // Update the confidence label text
+                self.confidence.text = "\(self.confidenceValue)"
+            }
+            
+            self.drawDetectionsOnPreview(detections: detections)
+        }
+    }
+    
+    func drawDetectionsOnPreview(detections: [VNRecognizedObjectObservation]) {
+        guard let image = self.photoImageView?.image else {
+            return
+        }
+        
+        let imageSize = image.size
+        let scale: CGFloat = 0
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, scale)
+
+        image.draw(at: CGPoint.zero)
+
+        for detection in detections {
+            
+            print(detection.labels.map({"\($0.identifier) confidence: \($0.confidence)"}).joined(separator: "\n"))
+            print("------------")
+            
+            let boundingBox = detection.boundingBox
+            let rectangle = CGRect(x: boundingBox.minX*image.size.width, y: (1-boundingBox.minY-boundingBox.height)*image.size.height, width: boundingBox.width*image.size.width, height: boundingBox.height*image.size.height)
+            UIColor(red: 0, green: 1, blue: 0, alpha: 0.4).setFill()
+            UIRectFillUsingBlendMode(rectangle, CGBlendMode.normal)
+        }
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.photoImageView?.image = newImage
+    }
+}
+
+extension ViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        self.confidence.text = "I'm not sure"
+
+        self.photoImageView?.image = image
+        updateDetections(for: image)
+    }
+}
---
+++ Kaspi-Logo.v1i.createml/README.dataset.txt
@@ -0,0 +1,6 @@
+# Kaspi-Logo > 2024-04-23 10:53am
+https://universe.roboflow.com/rako-fkx9u/kaspi-logo-uejqc
+
+Provided by a Roboflow user
+License: Public Domain
+
